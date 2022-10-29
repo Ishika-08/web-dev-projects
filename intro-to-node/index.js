bbb99f57ef3fd33d5a6a2dd7d867cc572ce16173
Ishika-08 <106862993+Ishika-08@users.noreply.github.com>
2022-10-29 11:29:03 +0530

    practice file for node
    server is listening at localhost:3000

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+const superheroes = require('superheroes');
+var myChoice = superheroes.random();
+console.log(myChoice);
+
+const supervillains = require('supervillains');
+var myChoice = supervillains.random();
+console.log("The villain is: " + myChoice);
